07444b444e66a4bb15aed0b6b654f32eed2b9c59
Your Name <your-email@domain.com>
2023-06-16 17:32:31 +0545

    fixing apiKey

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -10,15 +10,9 @@ export const WeatherApp = () => {
     try {
       const apiKey = "7c879a8831e9774036fb695c9b88c45a";
       const response = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`
       );
-      const data = response.data;
-      const temperatureCelsius = data.main.temp - 273.15; // Convert temperature from Kelvin to Celsius
-      const modifiedData = {
-        ...data,
-        main: { ...data.main, temp: temperatureCelsius },
-      }; // Update the temperature value
-      return modifiedData;
+      return response.data;
     } catch (e) {
       if (e instanceof Error) {
         return e.message;
@@ -64,7 +58,7 @@ export const WeatherApp = () => {
         <div className="Data">
           <h2>Weather Data:</h2>
           <p>City: {data.name}</p>
-          {data.main.temp && <p>Temperature: {data.main.temp.toFixed(2)}°C</p>}
+          {data.main.temp && <p>Temperature: {data.main.temp}°C</p>}
           {data.weather[0].main && <p>Condition: {data.weather[0].main}</p>}
           {data.weather[0].description && (
             <p>Description: {data.weather[0].description}</p>
